2a3193373ea2f44ba0733238524591e67aa0413f
Nehaa7255 <nehamas04@gmail.com>
2024-06-24 17:13:40 +0530

    Question 4 completed.

--- set2/script.js
+++ set2/script.js
@@ -59,3 +59,29 @@ const employeeData = [
     {name: "shyam", dept: "finance professional"},
     ];
     //console.log(greetEmployeeMessages(employeeData));
+
+    
+/*
+4. Write an ES6 function that takes an array of objects representing books and returns an array with only the titles of each book.
+const books = [
+ { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+ { title: 'To Kill a Mockingbird', author: 'Harper Lee' },
+ { title: '1984', author: 'George Orwell' },
+ { title: 'Pride and Prejudice', author: 'Jane Austen' },
+  / Your code here
+const titles = getBookTitles(books);
+console.log(titles); // Output: ['The Great Gatsby', 'To Kill a Mockingbird', '1984', 'Pr
+*/
+const getBookTitles=(book)=>{
+    return book.map(tit=>tit.title)
+    }
+    const books = [
+        { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+        { title: 'To Kill a Mockingbird', author: 'Harper Lee' },
+        { title: '1984', author: 'George Orwell' },
+        { title: 'Pride and Prejudice', author: 'Jane Austen' },
+    ]
+    const titles = getBookTitles(books);
+    //console.log(titles);
+    
+    
