6f4b39bb90f1602929dab0e89dda72e1f0f8b669
Nehaa7255 <nehamas04@gmail.com>
2024-06-24 17:12:20 +0530

    Question 2 and 3 completed.

--- set2/script.js
+++ set2/script.js
@@ -25,3 +25,37 @@ const people = [
    ];
    const names = getNames(people);
    //console.log(names);
+
+/*
+2. Given an array, write an ES6 function to return a new array with all the elements multiplied by 5.
+const numbers = [1, 2, 3, 4];
+/ Your code here
+console.log(multiplyByFive); // Output: [5, 10, 15, 20]
+*/
+const multiplyByFive=(number)=>{
+    return number.map(num=>num*5)
+}
+const numbers = [1, 2, 3, 4];
+//console.log(multiplyByFive(numbers));
+
+/*
+3. Given employee’s data, write an ES6 function which greets them with a personalized message for onboarding.
+const employeeData = [
+{name: "ram", dept: "marketer"},
+{name: "Radha", dept: "SDE"},
+{name: "shyam", dept: "finance professional"},
+]
+/ Your code here
+console.log(greetEmployeeMessages);
+/ Output: ['Hi ram we are glad to have you as a marketing', 'Hi Radha we are glad to hav
+*/
+const greetEmployeeMessages=(employeData)=>
+    employeData.map((employee)=> `Hi ${employee.name} we are glad to have you as ${employee.dept}ing,`
+);
+
+const employeeData = [
+    {name: "ram", dept: "marketer"},
+    {name: "Radha", dept: "SDE"},
+    {name: "shyam", dept: "finance professional"},
+    ];
+    //console.log(greetEmployeeMessages(employeeData));
