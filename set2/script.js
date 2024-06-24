b70da19949f8713477458c741f28b0a61d1a2abb
Nehaa7255 <nehamas04@gmail.com>
2024-06-24 14:20:54 +0530

    set 2 created.

---
+++ set2/script.js
@@ -0,0 +1,6 @@
+/*
+Instructions:
+Make use of .map(), .filter() and .find() methods.
+You can make use of basic methods such as .length, toLowerCase(), toUpperCase() if needed.
+Do NOT use for-loops.
+*/
