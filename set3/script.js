604e6382fdb8d1111768e56b66fb5f9f2e256ca1
Nehaa7255 <nehamas04@gmail.com>
2024-06-25 23:20:18 +0530

    Question 1 and 2 completed.

--- set3/script.js
+++ set3/script.js
@@ -28,3 +28,31 @@ const cars = [
    ]
    const toyotaCar = findToyotaCar(cars)
 //console.log(toyotaCar);
+
+
+/*
+2.  Write an ES6 function that takes an array of objects containing car information (make, model, year) and returns an array with only the cars 
+that were made after the year 2012.
+const cars = [
+ { make: 'Toyota', model: 'Corolla', year: 2010 },
+ { make: 'Honda', model: 'Civic', year: 2012 },
+ { make: 'Toyota', model: 'Camry', year: 2015 },
+ { make: 'Ford', model: 'Mustang', year: 2018 },
+]
+/ Your code here
+const getCars = getCarDetails(cars)
+console.log(getCars)
+/ Output: [{ make: "Toyota", model: "Camry", year: 2015 },
+            { make: "Ford", model: "Musta
+*/
+const getCarDetails=(car)=>{
+    return car.filter(details=>details.year>2012)
+}
+const cars1 = [
+    { make: 'Toyota', model: 'Corolla', year: 2010 },
+    { make: 'Honda', model: 'Civic', year: 2012 },
+    { make: 'Toyota', model: 'Camry', year: 2015 },
+    { make: 'Ford', model: 'Mustang', year: 2018 },
+   ]
+   const getCars = getCarDetails(cars1)
+//console.log(getCars)
