b5c5804146fe2ed54f565a3517517d55fb7bd022
Nehaa7255 <nehamas04@gmail.com>
2024-06-25 23:23:37 +0530

    Question 6 completed.

--- set3/script.js
+++ set3/script.js
@@ -142,4 +142,56 @@ const students = [
    const highGradeStudents = filterStudentsByGrade(students, 85)
    //console.log(highGradeStudents)
 
+   /*
+   6. Write an ES6 function that takes an array of objects representing
+    books with properties title, author and pageCount. Return the first
+     book object that has more than 500 pages.
+const books = [
+ {
+ title: 'The Lord of the Rings',
+ author: 'J.R.R. Tolkien',
+ pageCount: 1178,
+ },
+ { title: 'To Kill a Mockingbird', author: 'Harper Lee', pageCount: 281 },
+ {
+ title: "The Hitchhiker's Guide to the Galaxy",
+ author: 'Douglas Adams',
+ pageCount: 193,
+ },
+ {
+ title: 'The Name of the Wind',
+ author: 'Patrick Rothfuss',
+ pageCount: 662,
+},
+]
+/ Your code here
+const bookWithMoreThan500Pages = findBookWithMoreThan500Pages(books)
+console.log(bookWithMoreThan500Pages)
+/ Output: { title: "The Lord of the Rings", author: "J.R.R. Tolkien", pageCount: 1178 }
+*/
+const findBookWithMoreThan500Pages=(page)=>{
+    return page.find(book=>book.pageCount>500)
+}
+
+const books = [
+    {
+    title: 'The Lord of the Rings',
+    author: 'J.R.R. Tolkien',
+    pageCount: 1178,
+    },
+    { title: 'To Kill a Mockingbird', author: 'Harper Lee', pageCount: 281 },
+    {
+    title: "The Hitchhiker's Guide to the Galaxy",
+    author: 'Douglas Adams',
+    pageCount: 193,
+    },
+    {
+    title: 'The Name of the Wind',
+    author: 'Patrick Rothfuss',
+    pageCount: 662,
+   },
+   ]
+   const bookWithMoreThan500Pages = findBookWithMoreThan500Pages(books)
+//   console.log(bookWithMoreThan500Pages)
+
    
