7c8075536467758488d0ff5aebd1577a3aec51ff
Nehaa7255 <nehamas04@gmail.com>
2024-06-25 23:21:24 +0530

    Question 3 completed.

--- set3/script.js
+++ set3/script.js
@@ -56,3 +56,32 @@ const cars1 = [
    ]
    const getCars = getCarDetails(cars1)
 //console.log(getCars)
+
+
+/* 
+3.  Write an ES6 function that takes an array of objects representing
+ products with properties name, price, and category. Return the first 
+ product object that is in the category "electronics".
+const products = [
+ { name: 'Toothbrush', price: 29, category: 'health' },
+ { name: 'Coffee Maker', price: 99, category: 'home' },
+ { name: 'iPad', price: 799, category: 'electronics' },
+ { name: 'Smartwatch', price: 199, category: 'electronics' },
+]
+/ Your code here
+const electronicsProduct = findElectronicsProduct(products)
+console.log(electronicsProduct)
+/ Output: { name: "iPad", price: 799, category: "electronics" }
+*/
+const findElectronicsProduct=(categories)=>{
+    return categories.find(categ=>categ.category==='electronics')
+}
+const products = [
+    { name: 'Toothbrush', price: 29, category: 'health' },
+    { name: 'Coffee Maker', price: 99, category: 'home' },
+    { name: 'iPad', price: 799, category: 'electronics' },
+    { name: 'Smartwatch', price: 199, category: 'electronics' },
+   ]
+   const electronicsProduct = findElectronicsProduct(products)
+//console.log(electronicsProduct)
+
